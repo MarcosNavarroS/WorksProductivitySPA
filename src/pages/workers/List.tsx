5ad26b92f65190ccd544bc4f791e3c88c1724c33
Marcos Navarro <marcosok91@gmail.com>
2020-12-29 19:32:07 +0100

    Initial commit

---
+++ src/pages/workers/List.tsx
@@ -0,0 +1,26 @@
+﻿import React from 'react'
+import {CBadge, CCard, CCardBody, CCardHeader, CCol, CDataTable, CRow} from "@coreui/react";
+import usersData from "../../views/users/UsersData";
+
+const fields = ['Documento identidad','Nombre', 'role', 'status']
+
+const List = () => {
+  return (
+    <CRow>
+      <CCol>
+        <CCard accentColor="primary">
+          <CCardHeader>
+            Listado de trabajadores
+          </CCardHeader>
+          <CCardBody>
+            <CDataTable
+
+            />
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default List
